447b39963985a87774d5630adf97c6b494924dbc
PeachTreeOath <peachtreeoath@gmail.com>
2017-04-21 21:10:08 -0600

    Double jumps

--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -2,28 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     public float runSpeed;
     public float jumpForce;
+    public int allowedJumps;
 
     private Rigidbody2D body;
-    private bool isJumping;
+    private int usedJumps;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         body = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         float hSpeed = Input.GetAxisRaw("Horizontal") * runSpeed * Time.deltaTime;
         transform.Translate(new Vector2(hSpeed, 0));
 
-        if(Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && allowedJumps > usedJumps)
         {
-            isJumping = true;
-            body.AddForce(new Vector2(0, jumpForce),ForceMode2D.Impulse);
+            usedJumps++;
+            body.velocity = Vector2.zero;
+            body.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
         }
-	}
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag.Equals("Ground"))
+        {
+            usedJumps = 0;
+        }
+    }
 }
