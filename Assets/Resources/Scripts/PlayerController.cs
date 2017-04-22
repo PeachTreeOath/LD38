e0d3c68660b6112d35ee85b7d86b21e511083345
PeachTreeOath <peachtreeoath@gmail.com>
2017-04-21 21:03:41 -0600

    More scene changes

--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public float speed;
+    public float runSpeed;
+    public float jumpForce;
 
     private Rigidbody2D body;
+    private bool isJumping;
 
 	// Use this for initialization
 	void Start () {
@@ -15,7 +17,13 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        float hSpeed = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        float hSpeed = Input.GetAxisRaw("Horizontal") * runSpeed * Time.deltaTime;
         transform.Translate(new Vector2(hSpeed, 0));
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+            body.AddForce(new Vector2(0, jumpForce),ForceMode2D.Impulse);
+        }
 	}
 }
