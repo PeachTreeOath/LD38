a495815ee6fe2c7088e3e2051db594f139cc035f
Thundercleez <tmizas@gmail.com>
2017-04-22 18:08:42 -0400

    added vignette effect

--- Assets/Curve/PostEffect.cs
+++ Assets/Curve/PostEffect.cs
@@ -6,12 +6,14 @@ public class PostEffect : MonoBehaviour {
 
 	public float curveIntensity;
 	public Material mat;
+	public float vignettePower;
 
 	void OnRenderImage(RenderTexture src, RenderTexture dest)
 	{
 		mat.SetFloat("_ScreenWidth", Screen.width);
 		mat.SetFloat("_ScreenHeight", Screen.height);
 		mat.SetFloat("_CurveIntensity", curveIntensity);
+		mat.SetFloat("_VignettePower", vignettePower);
 		Graphics.Blit(src, dest, mat);
 	}
 }
