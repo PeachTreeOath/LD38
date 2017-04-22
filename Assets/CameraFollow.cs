55e22720d7c003c8b77dda8852d89b539f3da72e
PeachTreeOath <peachtreeoath@gmail.com>
2017-04-21 22:26:18 -0600

    Camera fix

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");	
+	}
+
+    void LateUpdate()
+    {
+        Vector2 playerPos = player.transform.position;
+        transform.position = new Vector3(playerPos.x, playerPos.y, transform.position.z);
+    }
+}
