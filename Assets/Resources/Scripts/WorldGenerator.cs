f9ee2856c3e9aa8390310d18cc05900bdbbaaa17
DarkFortress2\Elyk Yevrah <elykyevrah@gmail.com>
2017-04-22 00:44:13 -0500

    no message

--- Assets/Resources/Scripts/WorldGenerator.cs
+++ Assets/Resources/Scripts/WorldGenerator.cs
@@ -6,12 +6,15 @@ public class WorldGenerator : MonoBehaviour {
 
     public int depth;
     public int radius;
+    public float scale;
 
     public GameObject dirt;
 
     // Use this for initialization
     void Start()
     {
+        dirt.transform.localScale = new Vector3(dirt.transform.localScale.x * scale,
+            dirt.transform.localScale.y * scale, 1.0f);
         GenerateWorld();
     }
 
@@ -28,10 +31,10 @@ public class WorldGenerator : MonoBehaviour {
             for (int x = 0; x < radius; x++)
             {
                 GameObject goRight = Instantiate<GameObject>(dirt, transform);
-                goRight.transform.position = new Vector3(dirt.transform.position.x + x, dirt.transform.position.y + y);
+                goRight.transform.position = new Vector3(dirt.transform.position.x + (x * scale), dirt.transform.position.y + (y * scale));
 
                 GameObject goLeft = Instantiate<GameObject>(dirt, transform);
-                goLeft.transform.position = new Vector3(dirt.transform.position.x - x, dirt.transform.position.y + y);
+                goLeft.transform.position = new Vector3(dirt.transform.position.x - (x * scale), dirt.transform.position.y + (y * scale));
             }
         }
     }
