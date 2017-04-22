93f8a9bb86ce12d6e598f96c102185a3e329db95
DarkFortress2\Elyk Yevrah <elykyevrah@gmail.com>
2017-04-21 23:23:09 -0500

    no message

---
+++ Assets/Resources/Scripts/WorldGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGenerator : MonoBehaviour {
+
+    public int depth;
+    public int radius;
+
+    public GameObject dirt;
+
+    // Use this for initialization
+    void Start()
+    {
+        GenerateWorld();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void GenerateWorld()
+    {
+        for (int y = 0; y < depth; y++)
+        {
+            for (int x = 0; x < radius; x++)
+            {
+                GameObject goRight = Instantiate<GameObject>(dirt, transform);
+                goRight.transform.position = new Vector3(dirt.transform.position.x + x, dirt.transform.position.y + y);
+
+                GameObject goLeft = Instantiate<GameObject>(dirt, transform);
+                goLeft.transform.position = new Vector3(dirt.transform.position.x - x, dirt.transform.position.y + y);
+            }
+        }
+    }
+}
