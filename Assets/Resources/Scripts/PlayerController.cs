639a9d8b771c8f63b46674956e15c306b84ba0e7
PeachTreeOath <peachtreeoath@gmail.com>
2017-04-21 21:45:01 -0600

    Damage taking code and effect

--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -8,14 +8,26 @@ public class PlayerController : MonoBehaviour
     public float runSpeed;
     public float jumpForce;
     public int allowedJumps;
+    public int maxHealth;
 
     private Rigidbody2D body;
     private int usedJumps;
+    private int currentHealth;
+    private bool invincible;
+
+    private SpriteRenderer sprite;
+    private Material origMat;
+    private Material flashMat;
 
     // Use this for initialization
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        origMat = sprite.material;
+        flashMat = Resources.Load<Material>("Materials/WhiteFlashMat");
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -38,5 +50,41 @@ public class PlayerController : MonoBehaviour
         {
             usedJumps = 0;
         }
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (!invincible && col.gameObject.tag.Equals("Meteor"))
+        {
+            TakeDamage();
+        }
+    }
+
+    private void TakeDamage()
+    {
+        StartCoroutine(FlashWhite(.05f, .5f));
+        invincible = true;
+    }
+
+    private IEnumerator FlashWhite(float flashSpeed, float duration)
+    {
+        float elapsedTime = 0;
+        bool toggleFlash = false;
+        while (elapsedTime < duration)
+        {
+            toggleFlash = !toggleFlash;
+            if (toggleFlash)
+            {
+                sprite.material = flashMat;
+            }
+            else
+            {
+                sprite.material = origMat;
+            }
+            yield return new WaitForSeconds(flashSpeed);
+            elapsedTime += flashSpeed;
+        }
+        sprite.material = origMat;
+        invincible = false;
     }
 }
