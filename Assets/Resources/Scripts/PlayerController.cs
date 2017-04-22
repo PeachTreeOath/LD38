fb1e53409d4e2958ad80a0e992883ae04ff4cc75
PeachTreeOath <peachtreeoath@gmail.com>
2017-04-21 20:48:33 -0600

    Player movement

--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -6,13 +6,16 @@ public class PlayerController : MonoBehaviour {
 
     public float speed;
 
+    private Rigidbody2D body;
+
 	// Use this for initialization
 	void Start () {
-		
+        body = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Input.GetAxisRaw("Horizontal");
+        float hSpeed = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        transform.Translate(new Vector2(hSpeed, 0));
 	}
 }
