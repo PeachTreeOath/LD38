46c1b6dd24563fe1e5bdaa730c1300408c9ab1cf
Thundercleez <tmizas@gmail.com>
2017-04-22 13:52:44 -0400

    Added level transition.

---
+++ Assets/Resources/Scripts/NextLevelPoller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevelPoller : MonoBehaviour {
+
+	bool loadingLevel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!loadingLevel &&
+			Input.GetAxisRaw("Activate") > 0)
+		{
+			loadingLevel = true;
+			Debug.Log(Time.time + " go to next level");
+			//TODO: Load next level
+		}
+	}
+}
