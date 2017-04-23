ccd2f31f2c4e02c89b39c45ddf516bda7526d18a
PeachTreeOath <peachtreeoath@gmail.com>
2017-04-22 20:57:08 -0600

    Shop impl

--- Assets/Resources/Scripts/CraftButton.cs
+++ Assets/Resources/Scripts/CraftButton.cs
@@ -1,21 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CraftButton : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public static Sprite blueOrb;
+
+    public string itemName;
+
+    private void Awake()
+    {
+        blueOrb = Resources.Load<Sprite>("Textures/OrbBlue");
+    }
 
     public void OnClick()
     {
-        Debug.Log("OWIEJR");
+        ShopManager.Instance.PurchaseItem(itemName, this);
+    }
+
+    public void SetOrbs(int numOrbs)
+    {
+        Image[] images = GetComponentsInChildren<Image>();
+        int orbsPassed = 0;
+
+        for (int i = 0; i < images.Length; i++)
+        {
+            if (images[i].gameObject.GetInstanceID() != gameObject.GetInstanceID())
+            {
+                images[i].sprite = blueOrb;
+                orbsPassed++;
+            }
+            if(orbsPassed >= numOrbs)
+            {
+                break;
+            }
+        }
     }
 }
--- Assets/Resources/Scripts/ShopManager.cs
+++ Assets/Resources/Scripts/ShopManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,15 +10,49 @@ public class ShopManager : Singleton<ShopManager>
     private UpgradesPanelController shopPanel;
     private PlayerController player;
 
+    public int[] speedCosts = { 1, 3, 5 };
+    public int[] jumpCosts = { 1, 3, 5 };
+    public int[] armorCosts = { 1, 3, 5 };
+    public int[] radarCosts = { 1, 3, 5 };
+    public int[] magnetCosts = { 1, 3, 5 };
+    public int[] resourceCosts = { 1, 3, 5 };
+
+    public static string speedString = "Speed";
+    public static string jumpString = "Jump";
+    public static string armorString = "Armor";
+    public static string radarString = "Radar";
+    public static string magnetString = "Magnet";
+    public static string resourceString = "Resource";
+
+    public Dictionary<string, int[]> costMap;
+    public Dictionary<string, int> levelMap;
+
+    private bool debugOn = true;
+
     void Start()
     {
         player = GameObject.Find("Player").GetComponent<PlayerController>();
+        costMap = new Dictionary<string, int[]>();
+        costMap.Add(speedString, speedCosts);
+        costMap.Add(jumpString, jumpCosts);
+        costMap.Add(armorString, armorCosts);
+        costMap.Add(radarString, radarCosts);
+        costMap.Add(magnetString, magnetCosts);
+        costMap.Add(resourceString, resourceCosts);
+
+        levelMap = new Dictionary<string, int>();
+        levelMap.Add(speedString, 0);
+        levelMap.Add(jumpString, 0);
+        levelMap.Add(armorString, 0);
+        levelMap.Add(radarString, 0);
+        levelMap.Add(magnetString, 0);
+        levelMap.Add(resourceString, 0);
     }
 
-	protected override void Init()
-	{
+    protected override void Init()
+    {
 
-	}
+    }
 
     void Update()
     {
@@ -29,9 +64,9 @@ public class ShopManager : Singleton<ShopManager>
     }
 
     public void ToggleShop()
-    { 
+    {
         isActive = !isActive;
-		shopPanel = GameObject.Find("UpgradesPanel").GetComponent<UpgradesPanelController>();
+        shopPanel = GameObject.Find("UpgradesPanel").GetComponent<UpgradesPanelController>();
         shopPanel.ToggleShow(isActive);
     }
 
@@ -40,4 +75,31 @@ public class ShopManager : Singleton<ShopManager>
         return isActive;
     }
 
+    public void PurchaseItem(string itemName, CraftButton buttonCaller)
+    {
+        int level = levelMap[itemName];
+
+        if (level > 2)
+        {
+            return;
+        }
+
+        int cost = costMap[itemName][level];
+
+        if (debugOn)
+        {
+            Debug.Log("Resources: " + PlayerInventoryManager.Instance.PlayerResources + " Cost: " + cost);
+            levelMap[itemName]++;
+            buttonCaller.SetOrbs(levelMap[itemName]);
+        }
+        else
+        {
+            if (PlayerInventoryManager.Instance.PlayerResources >= cost)
+            {
+                levelMap[itemName]++;
+                PlayerInventoryManager.Instance.PlayerResources -= cost;
+                buttonCaller.SetOrbs(levelMap[itemName]);
+            }
+        }
+    }
 }
