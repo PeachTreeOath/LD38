f4b310f33a59336a2b21889d12139fd0a9133df6
PeachTreeOath <peachtreeoath@gmail.com>
2017-04-22 18:47:25 -0600

    Upgrade screen changes pt 1

--- Assets/Resources/Scripts/UpgradesPanelController.cs
+++ Assets/Resources/Scripts/UpgradesPanelController.cs
@@ -16,10 +16,14 @@ public class UpgradesPanelController : MonoBehaviour {
         if(show)
         {
             group.alpha = 1;
+            group.interactable = true;
+            group.blocksRaycasts = true;
         }
         else
         {
             group.alpha = 0;
+            group.interactable = false;
+            group.blocksRaycasts = false;
         }
     }
 
