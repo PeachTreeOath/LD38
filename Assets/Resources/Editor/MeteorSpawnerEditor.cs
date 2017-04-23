a9de37daa2f8294df66b01cb9fd77ca6152cc773
Unknown <wjackson2112@gmail.com>
2017-04-22 18:03:37 -0600

    Better Inspector for Meteor Spawner since Everyone likes to change that

---
+++ Assets/Resources/Editor/MeteorSpawnerEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MeteorSpawner))]
+public class MeteorSpawnerEditor : Editor {
+
+	public override void OnInspectorGUI(){
+		MeteorSpawner spawner = (MeteorSpawner)target;
+
+		EditorGUILayout.LabelField("Meteor Speed", EditorStyles.boldLabel);
+		spawner.speedMin = EditorGUILayout.FloatField("Min", spawner.speedMin);
+		spawner.speedMax = EditorGUILayout.FloatField("Max", spawner.speedMax);
+		EditorGUILayout.MinMaxSlider(ref spawner.speedMin, ref spawner.speedMax, 10, 50);
+
+		EditorGUILayout.LabelField("Spawner Details", EditorStyles.boldLabel);
+		spawner.spawnerWidth = EditorGUILayout.Slider("Width", spawner.spawnerWidth, 0, 20);
+		spawner.secondsPerMeteor = EditorGUILayout.Slider("Seconds/Meteor", spawner.secondsPerMeteor, 0, 2);
+		spawner.torqueRange = EditorGUILayout.Slider("Max Torque", spawner.torqueRange, 0, 50);
+		spawner.spawnerAngleRange = EditorGUILayout.Slider("Max Angle", spawner.spawnerAngleRange, 0, 6);
+		spawner.maxBlastSize = EditorGUILayout.Slider("Max Blast Size", spawner.maxBlastSize, 1, 5);
+
+		EditorUtility.SetDirty(spawner);
+	}
+}
--- Assets/Resources/Scripts/MeteorSpawner.cs
+++ Assets/Resources/Scripts/MeteorSpawner.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class MeteorSpawner : MonoBehaviour {
 
 	public GameObject meteorFab;
-	public float seconds_per_meteor;
-	public float speed_min;
-	public float speed_max;
-	public float torque_min;
-	public float torque_max;
+
+	[Header("Speed")]
+	public float speedMin;
+	public float speedMax;
+	[Space(10)]
+
+	[Range(0, 2)]
+	public float secondsPerMeteor;
+
+
+	[Range(0, 20)]
+	public float torqueRange;
+	[Range(0, 20)]
     public float spawnerWidth;
-    public float spawnerAngleVariance;
-    public int max_blastSize;
+    [Range(0, 5)]
+    public float spawnerAngleRange;
+
+    public float maxBlastSize;
 
 	private float prev_time;
 
@@ -26,7 +36,7 @@ public class MeteorSpawner : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		float curr_time = Time.time;
-		if(curr_time - prev_time > seconds_per_meteor)
+		if(curr_time - prev_time > secondsPerMeteor)
 		{
 			prev_time = curr_time;
 			genMeteor();
@@ -39,16 +49,16 @@ public class MeteorSpawner : MonoBehaviour {
 
 		GameObject meteor = Instantiate<GameObject>(meteorFab);
 		float x_offset = UnityEngine.Random.Range(-spawnerWidth, spawnerWidth);
-		float x_angle = Random.Range(-spawnerAngleVariance, spawnerAngleVariance);
+		float x_angle = Random.Range(-spawnerAngleRange, spawnerAngleRange);
 		meteor.transform.position = new Vector3(cam_pos.x + x_offset, cam_pos.y + 10, 1);
 		Meteor m = meteor.GetComponent<Meteor>();
        
-        m.blastRadius = Random.Range(0, max_blastSize);
+        m.blastRadius = Random.Range(0, maxBlastSize);
         m.transform.localScale = new Vector3(Mathf.Clamp(1.0f,3.0f,m.blastRadius),
             Mathf.Clamp(1.0f, 3.0f, m.blastRadius), 1.0f);
 
         m.moveDir = new Vector3(x_angle, -1).normalized;
-		m.moveSpeed = Random.Range(speed_min, speed_max);
-		m.torque = Random.Range(torque_min, torque_max);
+		m.moveSpeed = Random.Range(speedMin, speedMax);
+		m.torque = Random.Range(-torqueRange, torqueRange);
 	}
 }
