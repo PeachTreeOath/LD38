bedf1613abf40721bf13becbc3a3982cd05966e7
Unknown <wjackson2112@gmail.com>
2017-04-23 00:52:30 -0600

    Parallax the Background

---
+++ Assets/Resources/Scripts/BackgroundController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour {
+
+	public float scrollSpeed;
+	
+	// Update is called once per frame
+	void Update () {
+		float xPosition;
+		float camX = Camera.main.gameObject.transform.position.x;
+		float camY = Camera.main.gameObject.transform.position.y;
+
+		xPosition = camX - Mathf.Repeat((camX * scrollSpeed), gameObject.GetComponent<SpriteRenderer>().size.x / 2);
+		transform.position = new Vector3(xPosition, camY, 1);
+	}
+}
