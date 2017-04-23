23e88622d53939e56569c3a7b41cf5b3a6eed06d
Makenna Fish <makenna.g.f@gmail.com>
2017-04-23 14:33:08 -0600

    Meteor particles.

---
+++ Assets/Resources/Scripts/MeteorParticle.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class MeteorParticle : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.eulerAngles = new Vector3(0f, 0f, 0f);
+	}
+}
