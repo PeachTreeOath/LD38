e7c17377bf638c6c441ebdbcecdc9077fa66d1fd
PeachTreeOath <peachtreeoath@gmail.com>
2017-04-23 00:10:53 -0600

    Inv time change

--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -230,7 +230,7 @@ public class PlayerController : MonoBehaviour
     public void TakeDamage()
     {
         AudioManager.Instance.PlaySound("Damage", 0.5f);
-        StartCoroutine(FlashWhite(.05f, .5f));
+        StartCoroutine(FlashWhite(.05f, 1f));
         Vector2 hitDir = Vector2.zero;
         if (isFacingLeft)
         {
