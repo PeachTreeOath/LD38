8959e8173215afbbe06b4730bc2225eceb336044
Fangor <rfonger@gmail.com>
2017-04-23 21:47:36 -0600

    Adding Title Screen Music

---
+++ Assets/Resources/Scripts/TitleScreenMusic.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreenMusic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        AudioManager.Instance.PlayMusic("Title_Screen", .50f);
+    }
+	
+	
+}
